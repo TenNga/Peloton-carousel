5f3637396a199a8b76c08870164f78d7840d0901
Kevin Karma <tenngawang6080@gmail.com>
2024-11-13 18:14:26 -0500

    store all the neccessary icon and assets

---
+++ src/constants/appConstants.ts
@@ -0,0 +1,103 @@
+import { card } from "../types/globle";
+import strengthIcon from '../assets/strengthIcon.svg';
+import cyclingIcon from '../assets/cyclingIcon.svg';
+import yogaIcon from '../assets/yogaIcon.svg';
+import runningIcon from '../assets/runningIcon.svg';
+import boxingIcon from '../assets/boxingIcon.svg';
+import rowingIcon from '../assets/rowingIcon.svg';
+import meditationIcon from '../assets/meditationIcon.svg';
+import barreIcon from '../assets/barreIcon.svg';
+import stretchingIcon from '../assets/stretchingIcon.svg';
+import walkingIcon from '../assets/walkingIcon.svg';
+import pilatesIcon from '../assets/pilatesIcon.svg';
+
+export const cards: card[] = [
+  {
+    imageURL:
+      "https://res.cloudinary.com/peloton-cycle/image/fetch/dpr_1.0,f_auto,q_auto:good,w_600/https://images.ctfassets.net/7vk8puwnesgc/3Tygq87mpjCysd6dRomamP/d8a7b4f46a0c45eb3ff95a1a2e186d84/strength.png",
+    heading: "strength",
+    linkText: "preview strength classes",
+    link: "https://www.onepeloton.com/classes/strength",
+    workoutIcon: strengthIcon
+  },
+  {
+    imageURL:
+      "https://res.cloudinary.com/peloton-cycle/image/fetch/dpr_1.0,f_auto,q_auto:good,w_600/https://images.ctfassets.net/7vk8puwnesgc/6SpPJjMEkwBvt4dcuROUhM/0969da2fdbd85583e417f59e5fec0371/cycling.png",
+    heading: "cycling",
+    linkText: "preview cycling classes",
+    link: "https://www.onepeloton.com/classes/cycling",
+    workoutIcon: cyclingIcon
+  },
+  {
+    imageURL:
+      "https://res.cloudinary.com/peloton-cycle/image/fetch/dpr_1.0,f_auto,q_auto:good,w_600/https://images.ctfassets.net/7vk8puwnesgc/39RfW5BtElEcHNCRlSo6Df/c864ce9b567e24c3842f4ec0c907295d/yoga.png",
+    heading: "yoga",
+    linkText: "preview yoga classes",
+    link: "https://www.onepeloton.com/classes/yoga",
+    workoutIcon: yogaIcon
+  },
+  {
+    imageURL:
+      "https://res.cloudinary.com/peloton-cycle/image/fetch/dpr_1.0,f_auto,q_auto:good,w_600/https://images.ctfassets.net/7vk8puwnesgc/4Jc8Hc0m70MpNiVmo6qJTx/e4d5cda8bd22e8861fc6e846d01f2e63/running.png",
+    heading: "running",
+    linkText: "preview running classes",
+    link: "https://www.onepeloton.com/classes/running",
+    workoutIcon: runningIcon
+  },
+  {
+    imageURL:
+      "https://res.cloudinary.com/peloton-cycle/image/fetch/dpr_1.0,f_auto,q_auto:good,w_600/https://images.ctfassets.net/7vk8puwnesgc/1xxaCMlxguek1IOGongpIE/4aae8d7a1cc246cdf479174d47c00dbd/boxing.png",
+    heading: "boxing",
+    linkText: "preview boxing classes",
+    link: "https://www.onepeloton.com/classes/boxing",
+    workoutIcon: boxingIcon
+  },
+  {
+    imageURL:
+      "https://res.cloudinary.com/peloton-cycle/image/fetch/dpr_1.0,f_auto,q_auto:good,w_600/https://images.ctfassets.net/7vk8puwnesgc/4iDBWDsC4KYkpeMR6fVZYK/caa2b9fc508bb01ff6e8f27df0872541/rowing.png",
+    heading: "rowing",
+    linkText: "preview rowing classes",
+    link: "https://www.onepeloton.com/classes/rowing",
+    workoutIcon: rowingIcon
+  },
+  {
+    imageURL:
+      "https://res.cloudinary.com/peloton-cycle/image/fetch/dpr_1.0,f_auto,q_auto:good,w_600/https://images.ctfassets.net/7vk8puwnesgc/5MZALvSO3XxB4Cr6ac8FXP/728e707b749abd719d30614d1cb30ebe/meditation.png",
+    heading: "meditation",
+    linkText: "preview meditation classes",
+    link: "https://www.onepeloton.com/classes/meditation",
+    workoutIcon: meditationIcon
+  },
+  {
+    imageURL:
+      "https://res.cloudinary.com/peloton-cycle/image/fetch/dpr_1.0,f_auto,q_auto:good,w_600/https://images.ctfassets.net/7vk8puwnesgc/4ZnglqAN0rv0PTiXqPIMcS/afbeca60f9edd33d566d0a33fcd529a1/barre.png",
+    heading: "barre",
+    linkText: "preview barre classes",
+    link: "https://www.onepeloton.com/classes/barre",
+    workoutIcon: barreIcon
+  },
+  {
+    imageURL:
+      "https://res.cloudinary.com/peloton-cycle/image/fetch/dpr_1.0,f_auto,q_auto:good,w_600/https://images.ctfassets.net/7vk8puwnesgc/GWFCVD0x5miCw8s6hPlC2/6d276c1c1b2d21c6988643e5104d7f1d/stretching.png",
+    heading: "stretching",
+    linkText: "preview stretching classes",
+    link: "https://www.onepeloton.com/classes/stretching",
+    workoutIcon: stretchingIcon
+  },
+  {
+    imageURL:
+      "https://res.cloudinary.com/peloton-cycle/image/fetch/dpr_1.0,f_auto,q_auto:good,w_600/https://images.ctfassets.net/7vk8puwnesgc/4U2KaAhIDv0sXJAE6zzoPE/9d00b065573fc4a083b5718d9be727c1/walking.png",
+    heading: "walking",
+    linkText: "preview walking classes",
+    link: "https://www.onepeloton.com/classes/walking",
+    workoutIcon: walkingIcon
+  },
+  {
+    imageURL:
+      "https://res.cloudinary.com/peloton-cycle/image/fetch/dpr_1.0,f_auto,q_auto:good,w_600/https://images.ctfassets.net/7vk8puwnesgc/1w5iVdAQ9lHnn63MbJkrwc/a76bf3c3ca0561cac94fde2a78b9ca0e/pilates.png",
+    heading: "pilates",
+    linkText: "preview pilates classes",
+    link: "https://www.onepeloton.com/classes/pilates",
+    workoutIcon: pilatesIcon
+  },
+];
---
+++ src/types/globle.ts
@@ -0,0 +1,7 @@
+export interface card {
+    imageURL: string,
+    heading: string,
+    link: string,
+    linkText: string,
+    workoutIcon: string
+}
